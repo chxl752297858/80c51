ad71e8bb5718d09da3b1070ad630074d0fbedaa6
chxl752297858 <chxl752297858@163.com>
2012-12-02 23:09:50 +0800

    Revert "8051"
    This reverts commit 0008c78a78804d22ccd26dad5bcb86245cd31da8.

--- 12864并行驱动时钟程序/12864串行完整驱动程序模块化编程/defi.h
+++
@@ -1,6 +0,0 @@
-#ifndef __DEFI_H__
-#define __DEFI_H__
-#define uint unsigned int
-#define uchar unsigned char
-
-#endif
--- 1302+数码管显示+18b20+按键可调时间程序/12c5a.h
+++
@@ -1,9 +0,0 @@
- #ifndef __12C5A_H__
-#define __12C5A_H__
-
-/*  BYTE Registers  */
-
-sfr CLK_DIV  = 0x97;
-sfr IE2  = 0xAF;
-
-#endif
